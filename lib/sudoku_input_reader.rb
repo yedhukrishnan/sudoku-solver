f551d9d8d5464c24f7ba8f2b8ffd8067d2667312
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2016-08-16 22:00:30 +0530

    Add sudoku input reader
     - Read input from user in the form of string and convert it into hash
       to create sudoku object

---
+++ lib/sudoku_input_reader.rb
@@ -0,0 +1,17 @@
+require './lib/sudoku'
+
+class SudokuInputReader
+  def create_sudoku
+    puts "Input the initial sudoku :"
+    initial_sudoku_hash = Hash.new
+    (0..8).each do |row|
+      initial_sudoku_hash.merge! row_hash(row)
+    end
+    Sudoku.new(initial_sudoku_hash)
+  end
+
+  def row_hash row
+    line = gets
+    Hash[line.split(' ').each_with_index.collect { |value, col| [[row, col], value.to_i] }]
+  end
+end
